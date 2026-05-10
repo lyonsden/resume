bfee64ca8a65bc278000854a64043e565f5a37b6
Michael Lyons <lyonsden@Michaels-MacBook-Pro.local>
2026-05-10 12:42:44 -0400

    Fix asset paths (base: '/')

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://shadetreegeek.com',
-  base: './',
+  base: '/',
   outDir: './docs',
   output: 'static',
 });
